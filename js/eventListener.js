5691fb3477a6399e79d42987410e8445491a21ff
Tatas Fachrul <tatas@sinbad.co.id>
2020-11-09 10:27:15 +0700

    first commit

---
+++ js/eventListener.js
@@ -0,0 +1,8 @@
+const element = document.getElementById('card-1')
+element.addEventListener('click', function () {
+  alert('EventListener Clicked!')
+})
+
+function showAlert(){
+  alert('Button Clicked!')
+}
